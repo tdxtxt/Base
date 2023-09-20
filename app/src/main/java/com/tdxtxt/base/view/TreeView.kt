5e9c015b92124a2cfa6bd25faec9208460ec2b95
唐德祥 <tangdexiang@changan.com>
2023-09-20 10:04:48 +0800

    viewbinding支持自定义view

---
+++ app/src/main/java/com/tdxtxt/base/view/TreeView.kt
@@ -0,0 +1,33 @@
+package com.tdxtxt.base.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.LinearLayout
+import com.tdxtxt.base.R
+import com.tdxtxt.base.databinding.LayoutAppTreeviewBinding
+import com.tdxtxt.baselib.ui.viewbinding.IViewBinding
+
+/**
+ * <pre>
+ *     author : tangdexiang
+ *     time   : 2023-09-20
+ *     desc   :
+ * </pre>
+ */
+class TreeView: LinearLayout, IViewBinding<LayoutAppTreeviewBinding> {
+    constructor(context: Context): super(context) { initView(context) }
+    constructor(context: Context, attrs: AttributeSet?): super(context, attrs) { initView(context) }
+
+    private fun initView(context: Context){
+        orientation = VERTICAL
+        LayoutInflater.from(context).inflate(R.layout.layout_app_treeview, this, true)
+        setViewBindingRoot(this)
+
+        viewbindingOrNull()?.tv1?.text = "TextView1"
+        viewbindingOrNull()?.tv2?.text = "TextView2"
+    }
+    override fun viewbind(rootView: View) = LayoutAppTreeviewBinding.bind(rootView)
+
+}
--- lib_common/src/main/java/com/tdxtxt/baselib/ui/viewbinding/IViewBinding.kt
+++ lib_common/src/main/java/com/tdxtxt/baselib/ui/viewbinding/IViewBinding.kt
@@ -2,6 +2,7 @@ package com.tdxtxt.baselib.ui.viewbinding
 
 import android.view.View
 import androidx.viewbinding.ViewBinding
+import com.tdxtxt.baselib.R
 import com.tdxtxt.baselib.dialog.BottomBaseDialog
 import com.tdxtxt.baselib.dialog.CenterBaseDialog
 import com.tdxtxt.baselib.ui.BaseActivity
@@ -12,7 +13,8 @@ import com.tdxtxt.baselib.ui.BaseFragment
  * <pre>
  *     author : tangdexiang
  *     time   : 2023/9/16
- *     desc   : viewbinding接口封装，注意使用不当会抛出如下的错误
+ *     desc   : 支持的基类：BaseActivity、BaseFragment、BottomBaseDialog、CenterBaseDialog、View，需重写viewbind方法进行绑定视图
+ *              自定义View中的使用：1.必须在调用viewbinding方法前手动调用setViewBindingRoot(view)；2.不允许使用setTag(R.id.tag_viewbinding, Any)占用ViewBindingWrapper对象保存的坑位
  * </pre>
  */
 interface IViewBinding<T: ViewBinding> {
@@ -27,8 +29,11 @@ interface IViewBinding<T: ViewBinding> {
             viewbindingWrapper.viewbinding = viewbind(rootView)
         }else if(this is CenterBaseDialog){
             viewbindingWrapper.viewbinding = viewbind(rootView)
+        }else if(this is View){
+            if(viewbindingWrapper == null) viewbindingWrapper = ViewBindingWrapper()
+            viewbindingWrapper?.viewbinding = viewbind(rootView)
         }else{
-            throw Throwable("你的界面必须是BaseActivity、BaseFragment、BottomBaseDialog、CenterBaseDialog的子类")
+            throw Throwable("你的界面必须是BaseActivity、BaseFragment、BottomBaseDialog、CenterBaseDialog、View的子类")
         }
     }
 
@@ -41,8 +46,26 @@ interface IViewBinding<T: ViewBinding> {
             return viewbindingWrapper.viewbinding as T
         }else if(this is CenterBaseDialog){
             return viewbindingWrapper.viewbinding as T
+        }else if(this is View){
+            return viewbindingWrapper?.viewbinding as T
         }else{
-            throw Throwable("你的界面必须是BaseActivity、BaseFragment、BottomBaseDialog、CenterBaseDialog的子类")
+            throw Throwable("你的界面必须是BaseActivity、BaseFragment、BottomBaseDialog、CenterBaseDialog、View的子类")
+        }
+    }
+
+    fun viewbindingOrNull(): T? {
+        if(this is BaseActivity){
+            return viewbindingWrapper.viewbinding as T?
+        }else if(this is BaseFragment){
+            return viewbindingWrapper.viewbinding as T?
+        }else if(this is BottomBaseDialog){
+            return viewbindingWrapper.viewbinding as T?
+        }else if(this is CenterBaseDialog){
+            return viewbindingWrapper.viewbinding as T?
+        }else if(this is View){
+            return viewbindingWrapper?.viewbinding as T?
+        }else{
+            throw Throwable("你的界面必须是BaseActivity、BaseFragment、BottomBaseDialog、CenterBaseDialog、View的子类")
         }
     }
 
@@ -55,6 +78,12 @@ interface IViewBinding<T: ViewBinding> {
             viewbindingWrapper.viewbinding = null
         }else if(this is CenterBaseDialog){
             viewbindingWrapper.viewbinding = null
+        }else if(this is View){
+            viewbindingWrapper?.viewbinding = null
         }
     }
 }
+
+private var View.viewbindingWrapper: ViewBindingWrapper<ViewBinding>?
+    get() = getTag(R.id.tag_viewbinding) as ViewBindingWrapper<ViewBinding>?
+    set(value) = setTag(R.id.tag_viewbinding, value)
